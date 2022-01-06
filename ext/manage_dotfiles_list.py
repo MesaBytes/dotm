97a0f54f4d5e60e9aa551c898ad5b849380b5367
senpai-10 <bmjfdrh@gmail.com>
2022-01-06 21:49:26 +0300

    rename parameter

--- ext/manage_dotfiles_list.py
+++ ext/manage_dotfiles_list.py
@@ -3,6 +3,6 @@ import json
 class Dotfiles_list_manager:
   ''' manage dotfiles list '''
   def list(self): pass
-  def add(self, path: str, dist: str): pass
+  def add(self, source: str, dist: str): pass
   def remove(self): pass
   def generate_id(self): pass
