100a327598fd58b79df5b7c0b30b3a79386e1fbe
senpai-10 <bmjfdrh@gmail.com>
2022-01-06 21:40:44 +0300

    ext/manage_dotfiles_list.py

---
+++ ext/manage_dotfiles_list.py
@@ -0,0 +1,8 @@
+import json
+
+class Dotfiles_list_manager:
+  ''' manage dotfiles list '''
+  def list(self): pass
+  def add(self, path: str, dist: str): pass
+  def remove(self): pass
+  def generate_id(self): pass
