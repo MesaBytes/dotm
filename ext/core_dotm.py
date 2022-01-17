0cf09505aa55f20cb45dd60267295d55c6e9ffce
senpai-10 <bmjfdrh@gmail.com>
2022-01-17 06:48:54 +0300

    check for destination folders

--- ext/core_dotm.py
+++ ext/core_dotm.py
@@ -10,6 +10,21 @@ from ext.manage_dotfiles_list import Dotfiles_list_manager
 from ext.manage_config import Config
 from ext.color import color
 
+config = Config()
+
+def create_destination_folders(list):
+  for item in list:
+    item_source = item["source"]
+    item_copy_to_destination = item["destination"]
+    dirname = os.path.dirname(item_copy_to_destination)
+    dotfiles_dir_path = config.get("main", "dotfiles_path")
+    
+    if not os.path.exists(dotfiles_dir_path+dirname):
+      answer = input(f"{dotfiles_dir_path+dirname} directory does not exists, \nDo you want to create it? [{color.bold('yes/no')}]: ")
+      if answer == "yes" or answer == "y":
+        os.mkdir(dotfiles_dir_path+dirname) 
+        print(f"[{color.light.green('+')}] Directory '% s' is created" % dirname)
+
 def copy_files(source, destination):
   if os.path.isfile(source):
     shutil.copy2(source, destination)
@@ -19,20 +34,16 @@ def copy_files(source, destination):
 def backup():
   ''' loop through dotfiles list and backup all files '''
   dotfiles_list_manager = Dotfiles_list_manager()
-  config = Config()
   dotfiles_list = dotfiles_list_manager.get_dotfiles_list()
   dotfiles_dir_path = config.get("main", "dotfiles_path")
+
+  create_destination_folders(dotfiles_list)
 
   with alive_bar(len(dotfiles_list)) as bar:
     for item in dotfiles_list:
       item_source = item["source"]
       item_copy_to_destination = item["destination"]
       dirname = os.path.dirname(item_copy_to_destination)
-      if not os.path.exists(dotfiles_dir_path+dirname):
-        answer = input(f"{dotfiles_dir_path+dirname} directory does not exists, \nDo you want to create it? [{color.bold('yes/no')}]: ")
-        if answer == "yes" or answer == "y":
-          os.mkdir(dotfiles_dir_path+dirname) 
-          print(f"[{color.light.green('+')}] Directory '% s' is created" % dirname)
-      
+
       copy_files(item_source, os.path.join(dotfiles_dir_path, item_copy_to_destination))
       bar()
