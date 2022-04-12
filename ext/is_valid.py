2dc793cf6373665339a9c6556e095965d1609a19
senpai-10 <bmjfdrh@gmail.com>
2022-04-12 08:38:58 +0300

    remove old file

--- ext/is_valid.py
+++
@@ -1,9 +0,0 @@
-from os import path
-
-def is_valid_dir(path_):
-  if not path.isdir(path_): return False
-  return True
-
-def is_valid_file(path_):
-  if not path.isfile(path_): return False
-  return True
