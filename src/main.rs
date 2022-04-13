d451240e8b692d3172778815e6bb566170fda3b5
senpai-10 <bmjfdrh@gmail.com>
2022-04-13 13:09:15 +0300

    main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+// TODO: write add command
+// TODO: write remove command
+// TODO: write backup command
+// TODO: write list command
+// TODO: write empty-list command
+// TODO: write version command
+// TODO: write help command
+// if no dotfiles.json is found create one in home dir
+
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().skip(1).collect();
+    let command = &args[0];
+
+    if command == "add" {
+        let source = &args[1];
+        let dest = &args[2];
+
+        println!("source {}, dest {}", source, dest);
+    }
+
+    println!("{:?}", args);
 }
