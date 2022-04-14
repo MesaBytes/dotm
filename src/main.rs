6cc66d0e4d088c7c25c9861c81a5cd1b9ef9786f
senpai-10 <bmjfdrh@gmail.com>
2022-04-14 08:11:44 +0300

    save fn

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,30 @@
 
 extern crate ncurses;
 use ncurses as nc;
+use uuid::Uuid;
 
 const REGULAR_PAIR: i16 = 0;
 const HIGHLIGHT_PAIR: i16 = 1;
+
+struct Dotfile {
+    id: String,
+    source: String,
+    destination: String,
+}
+
+fn save(dotfiles: &Vec<Dotfile>) {
+    let mut contents = String::new();
+
+    for dotfile in dotfiles.iter() {
+        contents.push_str(&dotfile.id);
+        contents.push(':');
+        contents.push_str(&dotfile.source);
+        contents.push(':');
+        contents.push_str(&dotfile.destination);
+        contents.push('\n');
+    }
+    std::fs::write("dotm.db", contents);
+}
 
 fn main() {
     nc::initscr();
@@ -23,18 +44,25 @@ fn main() {
     nc::init_pair(HIGHLIGHT_PAIR, nc::COLOR_BLACK, nc::COLOR_WHITE);
 
     let mut quit = false;
-    let mut paths = vec![
-        "1226::/home/senpai/.books.db::thome",
-        "2236::/home/senpai/.config/dotm/::.config",
-        "3",
-        "4",
-        "5",
-        "6",
-    ];
+    let mut dotfiles: Vec<Dotfile> = Vec::new();
     let mut curr_idx: usize = 0;
 
+    let uuid = Uuid::new_v4();
+
+    dotfiles.push(Dotfile {
+        id: uuid.to_string(),
+        source: String::from("/home/senpai/books.db"),
+        destination: String::from("home/"),
+    });
+
+    dotfiles.push(Dotfile {
+        id: uuid.to_string(),
+        source: String::from("/home/senpai/.bashrc"),
+        destination: String::from("home/"),
+    });
+
     while !quit {
-        for (index, path) in paths.iter().enumerate() {
+        for (index, dotfile) in dotfiles.iter().enumerate() {
             let pair = {
                 if curr_idx == index {
                     HIGHLIGHT_PAIR
@@ -42,9 +70,16 @@ fn main() {
                     REGULAR_PAIR
                 }
             };
+            let format = std::format!(
+                "{}\t{}\t{}",
+                dotfile.id,
+                dotfile.source,
+                dotfile.destination
+            );
+
             nc::attron(nc::COLOR_PAIR(pair));
             nc::mv(index as i32, 0);
-            nc::addstr(*path);
+            nc::addstr(&format);
             nc::attr_off(nc::COLOR_PAIR(pair));
         }
 
@@ -57,14 +92,14 @@ fn main() {
             'k' => {
                 if curr_idx == 0 {
                     // Move cursor to the end of the list
-                    curr_idx = paths.len() - 1;
+                    curr_idx = dotfiles.len() - 1;
                 } else {
                     // Move up
                     curr_idx -= 1;
                 }
             }
             'j' => {
-                if curr_idx == paths.len() - 1 {
+                if curr_idx == dotfiles.len() - 1 {
                     // Move cursor to the start of the list
                     curr_idx = 0;
                 } else {
@@ -73,7 +108,8 @@ fn main() {
                 }
             }
             'd' => {
-                paths.remove(curr_idx);
+                dotfiles.remove(curr_idx);
+                save(&dotfiles);
                 nc::clear();
             }
             _ => {}
