e3ea25364e2469bc105cb5ca850d47d5d50982d3
senpai-10 <bmjfdrh@gmail.com>
2022-04-24 20:03:47 +0300

    message inline with stdin read_line

--- src/input.rs
+++ src/input.rs
@@ -1,7 +1,9 @@
-pub fn input(message: &str) -> String {
-    println!("{}", message);
-    let mut ret = String::new();
+use std::io::Write;
 
+pub fn input(message: &str) -> String {
+    print!("{}", message);
+    let mut ret = String::new();
+    std::io::stdout().flush().unwrap();
     std::io::stdin()
         .read_line(&mut ret)
         .expect("Failed to read from stdin");
