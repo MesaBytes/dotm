d1ea85cd4bb656d3b7395af8ca5e17e56e90929e
senpai-10 <bmjfdrh@gmail.com>
2022-05-04 18:40:16 +0300

    better scrolling

--- src/ui.rs
+++ src/ui.rs
@@ -5,18 +5,28 @@ const REGULAR_PAIR: i16 = 0;
 const HIGHLIGHT_PAIR: i16 = 1;
 
 pub struct Ui {
+    max_rows: i32,
+    max_columns: i32,
+    cursor_position: usize,
+    max_menu_rows: usize,
     quit: bool,
 }
 
 impl Ui {
     pub fn new() -> Ui {
-        Ui { quit: false }
+        Ui {
+            max_rows: 0,
+            max_columns: 0,
+            cursor_position: 0,
+            max_menu_rows: 0,
+            quit: false,
+        }
     }
 
     pub fn init(&mut self) {
-        let mut max_rows: i32 = 0;
-        let mut max_columns: i32 = 0;
-        let mut cursor_position: usize = 0;
+        // let mut max_rows: i32 = 0;
+        // let mut max_columns: i32 = 0;
+        // let mut cursor_position: usize = 0;
 
         let window = nc::initscr();
 
@@ -27,8 +37,9 @@ impl Ui {
         nc::noecho();
         nc::cbreak();
         nc::raw();
+        nc::curs_set(nc::CURSOR_VISIBILITY::CURSOR_INVISIBLE);
 
-        getmaxyx(window, &mut max_rows, &mut max_columns);
+        getmaxyx(window, &mut self.max_rows, &mut self.max_columns);
 
         let mut test_items: Vec<i32> = Vec::new();
 
@@ -36,6 +47,7 @@ impl Ui {
             test_items.push(i);
         }
 
+        self.max_menu_rows = test_items.len() - 1;
         let mut last_index: i32 = 0;
         let mut page_number: i32 = 0;
 
@@ -48,7 +60,7 @@ impl Ui {
                 // }
 
                 let pair = {
-                    if cursor_position == current_index {
+                    if self.cursor_position == current_index {
                         HIGHLIGHT_PAIR
                     } else {
                         REGULAR_PAIR
@@ -57,19 +69,15 @@ impl Ui {
 
                 nc::attron(nc::COLOR_PAIR(pair));
                 nc::mv(current_index as i32, 0);
-                nc::addstr("hii");
+                nc::addstr(&item.to_string());
                 nc::attr_off(nc::COLOR_PAIR(pair));
             }
 
             nc::refresh();
 
             match nc::getch() as u8 as char {
-                'j' => {
-                    cursor_position += 1;
-                }
-                'k' => {
-                    cursor_position -= 1;
-                }
+                'j' => self.scroll(ScrollDirections::Down),
+                'k' => self.scroll(ScrollDirections::Up),
                 'q' => {
                     self.quit = true;
                 }
@@ -79,4 +87,24 @@ impl Ui {
 
         nc::endwin();
     }
+
+    fn scroll(&mut self, direction: ScrollDirections) {
+        match &direction {
+            ScrollDirections::Up => {
+                if self.cursor_position != 0 {
+                    self.cursor_position -= 1;
+                }
+            }
+            ScrollDirections::Down => {
+                if self.cursor_position != self.max_menu_rows {
+                    self.cursor_position += 1;
+                }
+            }
+        }
+    }
+}
+
+enum ScrollDirections {
+    Up,
+    Down,
 }
